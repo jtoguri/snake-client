1cc8ad59003eca78848caca788d6fd7576dde671
Joshua Toguri <joshua.toguri@gmail.com>
2022-02-11 12:15:39 -0500

    mapped movment keys to send the correct data to the server

--- input.js
+++ input.js
@@ -1,6 +1,10 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -12,6 +16,17 @@ const setupInput = function () {
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  }
+
+  const mvmntKeys = {
+    w: "Move: up",
+    a: "Move: left",
+    s: "Move: down",
+    d: "Move: right"
+  }
+
+  if (mvmntKeys[key]) {
+    connection.write(mvmntKeys[key]);
   }
 };
 
